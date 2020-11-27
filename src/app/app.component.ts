32c258bd349f2693693f83ca353f83f663008b18
Blessing <bntoko@gmail.com>
2020-11-27 10:48:47 +0200

    added plane, sphere and cube || added ambient lighting || added dat.GUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui';
 
 @Component({
   selector: 'app-root',
@@ -38,6 +39,53 @@ export class AppComponent implements OnInit{
     texture.magFilter = THREE.NearestFilter;
     const repeats = planeSize / 2;
     texture.repeat.set(repeats, repeats);
+
+    {
+      const cubeSize = 4;
+      const cubeGeo = new THREE.BoxBufferGeometry(cubeSize, cubeSize, cubeSize);
+      const cubeMat = new THREE.MeshPhongMaterial({
+        color: '#8ac'
+      });
+      const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+      mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
+      this.scene.add(mesh);
+    }
+
+    {
+      const sphereRadius = 3;
+      const sphereWidthDiv = 32;
+      const sphereHeightDiv = 16;
+      const sphereGeo = new THREE.SphereBufferGeometry(sphereRadius, sphereWidthDiv, sphereHeightDiv);
+      const sphereMat = new THREE.MeshPhongMaterial({
+        color: '#ca8'
+      });
+      const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+      mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
+      this.scene.add(mesh);
+    }
+
+    const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshPhongMaterial({
+      map: texture,
+      side: THREE.DoubleSide
+    });
+    const planeMesh = new THREE.Mesh(planeGeo, planeMat);
+    planeMesh.rotation.x = Math.PI * -.5;
+    this.scene.add(planeMesh);
+
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.AmbientLight(color, intensity);
+    this.scene.add(light);
+
+    const animate = () => {
+
+      this.controls.update();
+
+      this.renderer.render(this.scene, this.camera);
+      requestAnimationFrame(animate)
+    }
+    requestAnimationFrame(animate);
 
   }
 }
