2a645267c8f388c343e5f55271b20bc0d9d28c63
Blessing <bntoko@gmail.com>
2020-11-27 10:26:51 +0200

    added checkerboard texture and added repeating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent implements OnInit{
   });
   public camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 100);
   public controls = new OrbitControls(this.camera, this.renderer.domElement);
+  public textureLoader = new THREE.TextureLoader();
 
   ngOnInit(){
     this.init();
@@ -24,9 +25,19 @@ export class AppComponent implements OnInit{
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
 
+    this.controls.target.set(0, 5, 0);
     this.controls.update();
 
     this.camera.position.set(0, 10, 20);
+
+    const planeSize = 40;
+
+    const texture = this.textureLoader.load('./assets/checker.png');
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.magFilter = THREE.NearestFilter;
+    const repeats = planeSize / 2;
+    texture.repeat.set(repeats, repeats);
 
   }
 }
